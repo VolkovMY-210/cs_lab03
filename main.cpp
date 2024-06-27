05c04a92e69cce0c96cd479ca8a8a00e3c75ee67
VolkovMY <maxiemalle@yandex.ru>
2024-06-27 21:27:17 +0300

    code: добавлены магические консанты

--- main.cpp
+++ main.cpp
@@ -93,6 +93,44 @@ show_histogram_text(const vector<size_t>& bins, size_t bin_count, size_t number_
         cout << endl;
     }
 }
+void svg_text(double left, double baseline, string text) {
+    cout << "<text x='" << left << "' y='" << baseline << "'>" << text << "</text>\n";
+}
+
+void svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox= '0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+
+void
+svg_rect(double x, double y, double width, double height) {
+    cout << "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "' />";
+}
+
+void
+svg_end() {
+    cout << "</svg>\n";
+}
+
+void
+show_histogram_svg(const vector<size_t>& bins) {
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+    
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+    svg_text(TEXT_LEFT, TEXT_BASELINE, to_string(bins[0]));
+    svg_rect(TEXT_WIDTH, 0, bins[0] * BLOCK_WIDTH, BIN_HEIGHT);
+    svg_end();
+}
 
 int main() {
     
@@ -107,7 +145,8 @@ int main() {
     cin >> bin_count;
     const auto bins = make_histogram(bin_count, numbers, number_count);
     
-    show_histogram_text(bins, bin_count, number_count);
+//    show_histogram_text(bins, bin_count, number_count);
+    show_histogram_svg(bins);
     
     return 0;
 }
